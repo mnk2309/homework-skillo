974cb67a95eaa640271476d8fb8ea3c4ded9ffaa
mnk2309 <mihail.n.kirkov@gmail.com>
2024-01-31 14:26:59 +0200

    a

--- Homewok_functions/04-homework-Problem2.py
+++ Homewok_functions/04-homework-Problem2.py
@@ -1,3 +1,16 @@
-from calculator-p2 import add, subtract, multiply, divide
+def simple_calculator(x, y, operator):
+    if operator == '+':
+        return x + y
+    elif operator == '-':
+        return x - y
+    elif operator == '*':
+        return x * y
+    elif operator == '/':
+        if y != 0:  # Avoid division by zero
+            return x / y
+        else:
+            raise ValueError("Division by zero is undefined.")
 
 
+result = simple_calculator(300, 12, '-')
+print(result)
